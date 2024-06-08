b9bad6c6b0271b4404f85a1894b45eae468bf577
Filip Jakovina <fj53514@fer.hr>
2024-06-08 18:16:18 +0200

    Initial commit

---
+++ ModeratorAplicationApp/ModeratorAplicationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModeratorAplicationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
